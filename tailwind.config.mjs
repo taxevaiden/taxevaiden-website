fa05ae16ddcfd10b1bae16f63d5f713438fa1347
aiden <aideniscool473@gmail.com>
2024-11-01 19:03:22 -0400

    you don't wanna see my dark side

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
 	theme: {
-		extend: {},
+		fontFamily: {
+			'sans': ['MS PGothic', 'system-ui', 'sans-serif'],
+			'mono': ['MS PGothic', 'system-ui'],
+		},
+
+		extend: {
+			colors: {
+				default: {
+					light: {
+						bg1: '#1c1c1c',
+						bg2: '#2c2c2e',
+
+						div: 'white',
+						border: 'black',
+
+						text: 'black',
+
+						a: "#0d5c8d",
+						ahover: "#0b78bc",
+					},
+
+					dark: {
+						bg1: '#1c1c1c',
+						bg2: '#2c2c2e',
+
+						div: '#19191a',
+						border: 'white',
+
+						text: 'white',
+						
+						a: "#8ab7d4",
+						ahover: "#93cbed",
+					}
+				},
+
+
+			},
+
+			boxShadow: {
+				'd': '0 10px rgba(0,0,0,0.5)',
+			}
+		},
 	},
 	plugins: [],
 }
