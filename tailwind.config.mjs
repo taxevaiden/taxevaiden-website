a2d6b9d4b46aaec7618fb7c97b295aa0ae75161c
aiden <aideniscool473@gmail.com>
2024-11-10 15:01:54 -0500

    aero yummy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
 
 	theme: {
 		fontFamily: {
-			'sans': ['MS Gothic', 'DotGothic16', 'system-ui', 'sans-serif'],
+			'sans': ['MS PGothic', 'DotGothic16', 'system-ui', 'sans-serif'],
 			'mono': ['MS Gothic', 'DotGothic16', 'system-ui'],
 		},
 
@@ -15,42 +15,22 @@ export default {
 						bg1: 'white',
 						bg2: '#ededed',
 
-						codebg1: '#1c1c1c',
-						codebg2: '#2c2c2e',
-
-						div: 'white',
-						div2: '#e8e8e8cc',
-						border: 'black',
+						div: 'rgba(245,245,255,0.025)',
+						pre: 'rgba(10,10,0,0.25)',
+						border: 'rgba(1,1,1,0.5)',
 
 						text: 'black',
 
 						a: "#0d5c8d",
 						ahover: "#0b78bc",
 					},
-
-					dark: {
-						bg1: '#1c1c1c',
-						bg2: '#2c2c2e',
-
-						codebg1: '#1c1c1c',
-						codebg2: '#2c2c2e',
-
-						div: '#19191a',
-						div2: '#111112cc',
-						border: 'white',
-
-						text: 'white',
-						
-						a: "#8ab7d4",
-						ahover: "#93cbed",
-					}
 				},
 
 
 			},
 
 			boxShadow: {
-				'd': '0 10px rgba(0,0,0,0.5)',
+				'd': ['0 0 8px rgba(0,0,0,0.5)', '0 0 4px rgba(0,0,0,1)', '0 0 2px rgba(0,0,0,1)', 'inset 0 0 16px rgba(255,255,255,0.75)', 'inset 0 0 8px rgba(255,255,255,0.75)'],
 			},
 
 			backgroundImage: {
@@ -58,7 +38,8 @@ export default {
 			},
 
 			dropShadow: {
-				'bigtitle': ['0px 10px rgba(0,0,0,0.05)', '1px 0px rgba(0,0,0,1)', '-1px 0px rgba(0,0,0,1)', '0px 1px rgba(0,0,0,1)', '0px -1px rgba(0,0,0,1)']
+				'bigtitle': ['0px 0px 8px rgba(0,0,0,0.125)', '1px 0px rgba(0,0,0,1)', '-1px 0px rgba(0,0,0,1)', '0px 1px rgba(0,0,0,1)', '0px -1px rgba(0,0,0,1)'],
+				't': ['0 0 8px rgb(255,255,255)', '0 0 2px rgba(255,255,255,0.5)']
 			}
 		},
 	},
