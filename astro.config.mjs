642faaf72f2ed21811510aa03d7ffb01a8f20c15
aiden <aideniscool473@gmail.com>
2024-12-21 23:51:19 -0500

    no more commenting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,18 @@ import { defineConfig, envField } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://taxevaiden.pages.dev",
-  adapter: cloudflare(),
 
-  env: {
-    schema: {
-      WORKER_URL: envField.string({
-        context: "client",
-        access: "public",
-        optional: true,
-      }),
-    },
-  },
+  // env: {
+  //   schema: {
+  //     WORKER_URL: envField.string({
+  //       context: "client",
+  //       access: "public",
+  //       optional: true,
+  //     }),
+  //   },
+  // },
 });
